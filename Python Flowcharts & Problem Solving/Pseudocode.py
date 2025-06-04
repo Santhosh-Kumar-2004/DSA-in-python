ec57ff2f9d5cce26c9d7770e8c82b4b028e11e2f
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-06-04 12:06:13 +0530

    Add files via upload

---
+++ Python Flowcharts & Problem Solving/Pseudocode.py
@@ -0,0 +1,81 @@
+'''
+1. TO ADD TWO NUMBERS
+
+Begin
+    Input FirstNumber
+    Input SecondNumber
+
+    SUM = FirstNumber + SecondNumber
+
+    print SUM
+
+End
+
+'''
+
+'''
+2. CHECK A NUMBER IS EVEN OR ODD
+
+Begin
+    Input Number
+
+    Number (devide by 2 and take Remainder) = 0
+    Then
+        Number is Even
+    Or
+        Number is Odd
+
+End
+
+'''
+
+'''
+3. POSITIVE, NEGATIVE OR ZERO
+
+Begin
+    Input Number
+
+    Check Number > 0
+    Then
+        Positive
+    
+    Or
+    Check Number < 0
+    Then 
+        Negative
+    
+    Finally
+    Check Number = 0
+    Then
+        Zero/Neutral Number
+
+End
+
+'''
+
+'''
+4. FIND THE LARGEST OF THREE NUMBERS
+
+Begin
+    Input Number 1
+    Input Number 2
+    Input Numnber 3
+
+    Check Number 1 > Number 2
+    If True, Then
+        Check Number 1 > Number 3
+            If True, Then
+                Number 1 is Largest
+    
+    Check Number 2 > Number 3
+    If True, Then
+        Number 2 is Largest
+    
+    If False, Then
+        Check Number 3 > Number 1
+        If True, Then
+        Number 3 is Largest.
+
+End
+
+'''
