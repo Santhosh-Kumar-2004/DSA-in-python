8a6c0b85755e8b5dae68417464ad6f28eaee76ce
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-06-09 10:32:46 +0530

    Add files via upload

---
+++ Searching & Sorting Algorithm/1. Introduction to Arrays.py
@@ -0,0 +1,20 @@
+# Python list
+l1 = []
+
+l1.append(1)
+l1.append(1.5)
+l1.append('python')
+
+print(l1)
+print(l1[0])
+
+#help statement used to informations about the Python
+help()
+
+#Python arrays
+import array as arr
+
+arr = arr.array('i',[1,2,3,4,5]) # i stands for integer data type
+print(arr)
+arr[0] = 10
+print(arr)
